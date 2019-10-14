adbb62cff3f37819a3a9fec63f1a8bdc0c528b63
Abdul Rehman Khan <abdulrehmankhan27061998@gmail.com>
2019-10-14 19:59:16 +0500

    modify home page

--- src/homepage/assets/js/testimonial_animation.js
+++ src/homepage/assets/js/testimonial_animation.js
@@ -2,22 +2,30 @@ $(document).ready(function() {
     console.log('[testimonial_animation][starts]');
     let currentFrame = 0;
 
-    const image_ = $("#testimonial_image");
-    const testimonial = $("#testimonial_content");
+    const image = $("#testimonial_image");
+    const name = $("#testimonial_content");
+    const content = $("#section3p");
+    const title = $("#section3_main .title");
 
     function stepAnimation() {
         console.log('[testimonial_animation][stepAnimation]');
-        image_.animate({ "width": "80px", "height": "80px", "margin-top": "50px", "opacity": "0.5" }, 1000);
-        testimonial.animate({ "opacity": "0" }, 1000, function() {
-            image_.css({ "background": "url(homepage/assets/images/testimonials/" + testimonials[currentFrame].name + ")", "background-size": "cover" });
-            testimonial.text(testimonials[currentFrame].content);
+        console.log('currentFrame -> ' + currentFrame);
+        image.animate({ "width": "80px", "height": "80px", "margin-top": "50px", "opacity": "0.5" }, 1000);
+        name.animate({ "opacity": "0" }, 1000, function() {
+            image.css({ "background": "url(images/testimonials/" + testimonials[currentFrame].image + ")", "background-size": "cover" });
+            name.text(testimonials[currentFrame].name);
+            content.text(testimonials[currentFrame].content);
+            title.text(testimonials[currentFrame].title);
+
             if (currentFrame == testimonials.length - 1) {
                 currentFrame = 0;
             } else {
                 currentFrame++;
             }
-            image_.animate({ "width": "180px", "height": "180px", "margin-top": "0px", "opacity": "1" }, 1000);
-            testimonial.animate({ "opacity": "1" }, 1000);
+            image.animate({ "width": "180px", "height": "180px", "margin-top": "0px", "opacity": "1" }, 1000);
+            name.animate({ "opacity": "1" }, 1000);
+            content.animate({ "opacity": "1" }, 1000);
+            title.animate({ "opacity": "1" }, 1000);
         });
     }
 
--- src/homepage/assets/js/testimonials.js
+++ src/homepage/assets/js/testimonials.js
@@ -1,4 +1,37 @@
-const testimonials = [
-    { "name": "sir_haroon.png", "content": "Dr. Haroon ur Rashid" },
-    { "name": "sir_haroon.png", "content": "Dr. Haroon ur Rashid" },
+const testimonials = [{
+        "image": "sir-haroon.jpg",
+        "name": "Dr. Haroon ur Rashid",
+        "content": "“Productivity is never an accident. It is always the result of commitment to excellence, intelligent planning and focussed effort.”",
+        "title": "Event Advisor",
+    },
+    {
+        "image": "abdul-wadood.jpg",
+        "name": "Abdul Wadood",
+        "content": "“PiON, one the biggest Olympiads of Pakistan, is full of opportunities for everyone to learn, to grow as an individual, to challenge yourself, to make new friends, to gain new experiences, awareness, knowledge and skills and have fun. Don’t miss the chance of participating in this momentous event. It is well said, “winning is something but participation is everything”.”",
+        "title": "Event Mentor",
+    },
+    {
+        "image": "zoha-rashid.jpg",
+        "name": "Zoha Rashid",
+        "content": "“When life gives you lemons, pop a battery and some wires and light a bulb. And if that’s not exciting enough, join a fluorescent world of creative engineers waiting to tap your minds with abstraction. Just kidding 💁🏻‍♀ See you at PiON’19.”",
+        "title": "Event Mentor",
+    },
+    {
+        "image": "ali-husnain-joyia.jpg",
+        "name": "Ali Husnain Joyia",
+        "content": "“Hard work, passion and zest are the essentials for making a project stand out, and I can think of no example of it other than the spirits of people who bring you PION.”",
+        "title": "Event Mentor",
+    },
+    {
+        "image": "hamza-javed.jpg",
+        "name": "Hamza javed",
+        "content": "“PION is a perfect example of the sheer hardwork and leadership capabilities of the amazing IEEE PIEAS SB. Wishing them the very best for this event.”",
+        "title": "Event Mentor",
+    },
+    {
+        "image": "ahab-zahoor.jpg",
+        "name": "Ahab Zahoor",
+        "content": "“I feel overwhelmed and extremely lucky to have worked with a best team. Team is highly instrumental and professional in guiding participants and planning for the events. PiON shaped me as a leader as i worked as a co convener last year and the skills developed during that time helped me to represent my country on an international ASME platform as a Student Regional Chair of Southwest Asia. I urge everyone to participate in this event and enjoy to the fullest.”",
+        "title": "Event Mentor",
+    },
 ]
